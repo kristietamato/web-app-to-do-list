ab6b5bb5e497700b052ad4126216e6ef8a196381
Kristie Nguyen <kristietamato@gmail.com>
2017-03-19 22:14:42 -0700

    replace console diplayToDos method with view.displayToDos

--- scripts/to-dos.js
+++ scripts/to-dos.js
@@ -1,39 +1,20 @@
 var toDoList = {
   toDos: [],
-  displayToDos: function() {
-    if (this.toDos.length === 0) {
-      console.log('Your to do list is empty.');
-    } else {
-        console.log('My to dos: ');
-        for(var count = 0; count < this.toDos.length; count++) {
-          // check if to do is completed
-          if(this.toDos[count].completed) {
-            console.log('(x) ', this.toDos[count].toDoText);
-          } else {
-            console.log('( ) ', this.toDos[count].toDoText);
-          }
-        }
-    }
-  },
   addToDo: function(toDoText) {
     this.toDos.push({
       toDoText: toDoText,
       completed: false
     });
-    this.displayToDos();
   },
   changeToDo: function(position, toDoText) {
     this.toDos[position].toDoText = toDoText;
-    this.displayToDos();
   },
   deleteToDo: function(position) {
     this.toDos.splice(position, 1);
-    this.displayToDos();
   },
   toggleCompleted: function(position) {
     var toDo = this.toDos[position];
     toDo.completed = !toDo.completed;
-    this.displayToDos();
   },
   toggleAll: function() {
     var totalToDos = this.toDos.length;
@@ -56,19 +37,15 @@ var toDoList = {
         this.toDos[count].completed = true;
       }
     }
-
-    this.displayToDos();
   }
 };
 
 var handlers = {
-  displayToDos: function() {
-    toDoList.displayToDos();
-  },
   addToDo: function() {
     var addToDoInput = document.getElementById("add-to-do-input");
     toDoList.addToDo(addToDoInput.value);
     addToDoInput.value = "";
+    view.displayToDos();
   },
   changeToDo: function() {
     var changeToDoPositionInput = document.getElementById("change-to-do-position-input");
@@ -76,19 +53,23 @@ var handlers = {
     toDoList.changeToDo(changeToDoPositionInput.valueAsNumber, changeToDoInput.value);
     changeToDoPositionInput.value = "";
     changeToDoInput.value = "";
+    view.displayToDos();
   },
   deleteToDo: function() {
     var deleteToDoPositionInput = document.getElementById("delete-to-do-position-input");
     toDoList.deleteToDo(deleteToDoPositionInput.valueAsNumber);
     deleteToDoPositionInput.value = "";
+    view.displayToDos();
   },
   toggleCompleted: function() {
     var toggleCompletedPositionInput = document.getElementById("toggle-completed-position-input");
     toDoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = "";
+    view.displayToDos();
   },
   toggleAll: function() {
     toDoList.toggleAll();
+    view.displayToDos();
   }
 };
 
@@ -98,6 +79,16 @@ var view = {
     toDosUl.innerHTML = "";
     for (var count = 0; count < toDoList.toDos.length; count++) {
       var toDoLi = document.createElement("li");
+      var toDo = toDoList.toDos[count];
+      var toDoTextWithCompletion = "";
+
+      if(toDo.completed) {
+        toDoTextWithCompletion = "(x) " + toDo.toDoText;
+      } else {
+        toDoTextWithCompletion = "( ) " + toDo.toDoText;
+      }
+
+      toDoLi.textContent = toDoTextWithCompletion;
       toDosUl.appendChild(toDoLi);
     }
   }
